961f030fb1902c056f515ea3783cf52f51a04644
cesreve <cesreve@gmail.com>
2026-02-17 21:25:01 +0100

    Add contracts and casting to mart models

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,7 +1,17 @@
-with customers as (
+with source as (
 
-    select * from {{ ref('stg_customers') }}
+    select * from {{ ref(
+        'stg_customers') }}
+
+),
+
+renamed as (
+
+    select
+        customer_id::varchar as customer_id,
+        customer_name::varchar as customer_name
+    from source
 
 )
 
-select * from customers
+select * from renamed
--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -1,7 +1,19 @@
-with orders as (
+with source as (
 
     select * from {{ ref("int_customers_orders") }}
 
+),
+
+renamed as (
+
+    select
+        order_id::varchar as order_id,
+        customer_id::varchar as customer_id,
+        order_total::number(38,2) as order_total,
+        tax_paid::number(38,2) as tax_paid,
+        ordered_at::timestamp_ntz as ordered_at
+    from source
+
 )
 
-select * from orders
+select * from renamed

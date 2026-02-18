e24fed7e4663c5d046f82cf5960c9fe29184aaab
cesreve <cesreve@gmail.com>
2026-02-18 09:31:35 +0100

    Add model versioning for dim_customers

---
+++ models/marts/dim_customers_v2.sql
@@ -0,0 +1,18 @@
+with source as (
+
+    select * from {{ ref(
+        'stg_customers') }}
+
+),
+
+renamed as (
+
+    select
+        customer_id::varchar as customer_id,
+        customer_name::varchar as customer_name,
+        'v2' as version
+    from source
+
+)
+
+select * from renamed
